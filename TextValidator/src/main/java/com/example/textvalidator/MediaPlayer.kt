6a452997e1fc15f714fb03d6a903e2fcc1f4766e
otabek <otabekibodov2@gmail.com>
2022-05-22 13:40:20 +0500

    secon commit

--- TextValidator/src/main/java/com/example/textvalidator/MediaPlayer.kt
+++ TextValidator/src/main/java/com/example/textvalidator/MediaPlayer.kt
@@ -1,7 +1,23 @@
 package com.example.textvalidator
 
+import android.content.Context
 import android.media.MediaPlayer
+import android.widget.Toast
+import java.net.URI
 
-object MediaPlayer:MediaPlayer() {
+object MediaPlayer:MediaPlayer.OnPreparedListener {
+    private var mediaPlayer: MediaPlayer?=null
+
+
+    fun playMusic(context: Context, url:String){
+            mediaPlayer = MediaPlayer()
+            mediaPlayer?.setDataSource(url)
+            mediaPlayer?.setOnPreparedListener(this)
+            mediaPlayer?.prepareAsync()
+
+    }
+    override fun onPrepared(p0: MediaPlayer?) {
+        mediaPlayer?.start()
+    }
 
 }
